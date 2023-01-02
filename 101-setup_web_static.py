25783635cbd0c1d413f409ca5deb62e462a4c2c8
Aaron <aarontamirat99@gmail.com>
2023-01-02 02:26:59 -0600

    Permission fix

--- 101-setup_web_static.py
+++ 101-setup_web_static.py
@@ -1,91 +1,29 @@
-# Configures a web server for deployment of web_static.
+# static deployment config using puppet
 
-# Nginx configuration file
-$nginx_conf = "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By ${hostname};
-    root   /var/www/html;
-    index  index.html index.htm;
-
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-
-    location /redirect_me {
-        return 301 http://youtube.com/;
-    }
-
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}"
-
-package { 'nginx':
-  ensure   => 'present',
-  provider => 'apt'
-} ->
-
-file { '/data':
-  ensure  => 'directory'
-} ->
-
-file { '/data/web_static':
-  ensure => 'directory'
-} ->
-
-file { '/data/web_static/releases':
-  ensure => 'directory'
-} ->
-
-file { '/data/web_static/releases/test':
-  ensure => 'directory'
-} ->
-
-file { '/data/web_static/shared':
-  ensure => 'directory'
-} ->
-
-file { '/data/web_static/releases/test/index.html':
-  ensure  => 'present',
-  content => "Holberton School Puppet\n"
-} ->
-
-file { '/data/web_static/current':
-  ensure => 'link',
-  target => '/data/web_static/releases/test'
-} ->
-
-exec { 'chown -R ubuntu:ubuntu /data/':
-  path => '/usr/bin/:/usr/local/bin/:/bin/'
+exec { 'apt-get-update':
+  command => '/usr/bin/env apt-get -y update',
 }
-
-file { '/var/www':
-  ensure => 'directory'
-} ->
-
-file { '/var/www/html':
-  ensure => 'directory'
-} ->
-
-file { '/var/www/html/index.html':
-  ensure  => 'present',
-  content => "Setting Web Static\n"
-} ->
-
-file { '/var/www/html/404.html':
-  ensure  => 'present',
-  content => "Ceci n'est pas une page\n"
-} ->
-
-file { '/etc/nginx/sites-available/default':
-  ensure  => 'present',
-  content => $nginx_conf
-} ->
-
-exec { 'nginx restart':
-  path => '/etc/init.d/'
+-> exec {'install':
+  command => '/usr/bin/env apt-get -y install nginx',
+}
+-> exec {'dir-1':
+  command => '/usr/bin/env mkdir -p /data/web_static/releases/test/',
+}
+-> exec {'dir-2':
+  command => '/usr/bin/env mkdir -p /data/web_static/shared/',
+}
+-> exec {'page-content':
+  command => '/usr/bin/env echo "Holberton School" > /data/web_static/releases/test/index.html',
+}
+-> exec {'link':
+  command => '/usr/bin/env ln -sf /data/web_static/releases/test /data/web_static/current',
+}
+-> exec {'config':
+  command => '/usr/bin/env sed -i "/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}" /etc/nginx/sites-available/default',
+}
+-> exec {'restart':
+  command => '/usr/bin/env service nginx restart',
+}
+-> exec {'owner':
+  command => '/usr/bin/env chown -R ubuntu:ubuntu /data',
 }
